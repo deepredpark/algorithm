c31559e11868322169f611f0b95c0047a944ea1c
Jinhong Park <deepredpark@gmail.com>
2021-10-18 00:27:04 +0900

    Update page217.py

--- book/coding-test/dynamic_programming/page217.py
+++ book/coding-test/dynamic_programming/page217.py
@@ -27,3 +27,23 @@ while queue:
       count[i] = count[v] + 1
 
 print(count[1])
+
+# solution - Dongbin Na
+# a[i] = min(a[i-1], a[i/2], a[i/3], a[i/5]) + 1
+
+x = int(input())
+d = [0] * 30001
+
+for i in range(2, x + 1):
+  d[i] = d[i - 1] + 1
+  if i % 2 == 0:
+    d[i] = min(d[i], d[i // 2] + 1)
+  if i % 3 == 0:
+    d[i] = min(d[i], d[i // 3] + 1)
+  if i % 5 == 0:
+    d[i] = min(d[i], d[i // 5] + 1)
+  
+print(d[x])
+
+
+
