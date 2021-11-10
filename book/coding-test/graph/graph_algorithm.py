b94518781cd635ab98495d5b250793162973dfa8
Jinhong Park <deepredpark@gmail.com>
2021-11-11 01:25:23 +0900

    Update graph_algorithm.py

--- book/coding-test/graph/graph_algorithm.py
+++ book/coding-test/graph/graph_algorithm.py
@@ -419,6 +419,106 @@
 #   print(i, end=' ')
 ##############################################################################
 # 7. LCA(lowest common ancestor) + upgrade version
+# BOJ p11437 LCA(lowest common ancestor) - O(NM)
+# import sys
+# from collections import deque
+# input = sys.stdin.readline
+
+# def lca(a, b):
+#   while degree[a] != degree[b]:
+#     if degree[a] > degree[b]:
+#       a = parent[a]
+#     else:
+#       b = parent[b]
+#   while a != b:
+#     a = parent[a]
+#     b = parent[b]
+#   return a
+
+# n = int(input())
+# graph = [[] for _ in range(n + 1)]
+# for _ in range(n - 1):
+#   a, b = map(int, input().split())
+#   graph[a].append(b)
+#   graph[b].append(a)
+
+# parent = [1] * (n + 1)
+# visited = [False] * (n + 1)
+# degree = [0] * (n + 1)
+
+# q = deque()
+# q.append(1)
+# visited[1] = True
+# while q:
+#   now = q.popleft()
+#   for i in graph[now]:
+#     if not visited[i]:
+#       q.append(i)
+#       visited[i] = True
+#       degree[i] = degree[now] + 1
+#       parent[i] = now
+
+# m = int(input())
+# for _ in range(m):
+#   a, b = map(int, input().split())
+#   print(lca(a, b))
+
+# BOJ p11438 LCA2 upgrade version - O(MlogN)
+# import sys
+# from collections import deque
+# input = sys.stdin.readline
+# LOG = 21 #2^20 == 1000000
+
+# n = int(input())
+# graph = [[] for _ in range(n + 1)]
+# for _ in range(n - 1):
+#   a, b = map(int, input().split())
+#   graph[a].append(b)
+#   graph[b].append(a)
+
+# # parent table expansion by LOG
+# parent = [[0] * LOG for _ in range(n + 1)]
+# visited = [False] * (n + 1)
+# degree = [0] * (n + 1)
+
+# def bfs(root, depth):
+#   q = deque()
+#   q.append(root)
+#   visited[root] = True
+#   degree[root] = depth
+#   while q:
+#     now = q.popleft()
+#     for i in graph[now]:
+#       if not visited[i]:
+#         q.append(i)
+#         visited[i] = True
+#         degree[i] = degree[now] + 1
+#         parent[i][0] = now
+
+# def set_parent():
+#   bfs(1, 0)
+#   for i in range(1, LOG):
+#     for j in range(1, n + 1):
+#       parent[j][i] = parent[parent[j][i - 1]][i - 1]
+
+# def lca(a, b):
+#   if degree[a] > degree[b]:
+#     a, b = b, a
+#   for i in range(LOG - 1, -1, -1):
+#     if degree[b] - degree[a] >= (1 << i):
+#       b = parent[b][i]
+#   if a == b:
+#     return a
+#   for i in range(LOG - 1, -1, -1):
+#     if parent[a][i] != parent[b][i]:
+#       a = parent[a][i]
+#       b = parent[b][i]
+#   return parent[a][0]
+
+# set_parent()
+
+# m = int(input())
+# for _ in range(m):
+#   a, b = map(int, input().split())
+#   print(lca(a, b))
 ##############################################################################
-
-
